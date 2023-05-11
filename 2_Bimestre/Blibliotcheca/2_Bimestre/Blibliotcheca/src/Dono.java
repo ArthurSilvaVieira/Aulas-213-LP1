d8619ed9c42d560cb42a1633b70683dee3eb7b50
ArthurSilvaVieira <126774200+ArthurSilvaVieira@users.noreply.github.com>
2023-05-11 09:12:57 -0300

    Add files via upload

---
+++ 2_Bimestre/Blibliotcheca/2_Bimestre/Blibliotcheca/src/Dono.java
@@ -0,0 +1,25 @@
+public class Dono {
+    private String nome;
+    private int cpf;
+
+    public Dono(String nome, int cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
